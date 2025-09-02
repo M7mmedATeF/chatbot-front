d249a236eb767663be514ba9d7d1cea6e93b24fa
M7mmeDATeF <mohammed.atef.ewais@gmail.com>
2025-09-02 18:10:45 +0300

    admin layout

--- src/view/layouts/DashboardLayout/DashboardLayout.tsx
+++ src/view/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,12 +1,53 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 import "./DashboardLayout.css";
+import Button from "../../components/Button/Button";
+import {
+  AiOutlineArrowLeft,
+  AiOutlineDashboard,
+  AiOutlineRobot,
+  AiOutlineSetting,
+} from "react-icons/ai";
 
 const DashboardLayout = () => {
   return (
     <section className="dashboard-layout">
-      <nav>nav</nav>
-      <main>
-        main
+      <nav className="side-nav glass-bg">
+        <Link to="/">
+          <h1>Mcp-ChatBot</h1>
+        </Link>
+
+        <ul>
+          <li>
+            <Button href="/">
+              <AiOutlineDashboard />
+              Dashboard
+            </Button>
+          </li>
+          <li>
+            <Button href="/agents">
+              <AiOutlineRobot />
+              Agents
+            </Button>
+          </li>
+          <li>
+            <Button href="/settings">
+              <AiOutlineSetting />
+              Settings
+            </Button>
+          </li>
+          <li>
+            <Button href="/logout" theme="danger">
+              Logout
+            </Button>
+          </li>
+        </ul>
+      </nav>
+      <main className="glass-bg">
+        <header className="glass-bg">
+          <Button>
+            <AiOutlineArrowLeft />
+          </Button>
+        </header>
         <Outlet />
       </main>
     </section>
