a6f0a4a4792445f1035661cb04e989e0cecb5ebb
M7mmeDATeF <mohammed.atef.ewais@gmail.com>
2025-09-07 15:44:52 +0300

    fix issues

--- src/hooks/useListConfigs.ts
+++ src/hooks/useListConfigs.ts
@@ -3,15 +3,10 @@ import {
   listConfigsQuery,
   type ListConfigsResponse,
 } from "../services/Queries/Config.gql";
-import { toast } from "react-toastify";
 
 export const useListConfigs = () => {
   return useQuery<ListConfigsResponse>({
     queryKey: ["configs"],
     queryFn: listConfigsQuery,
-    onError: (error: any) => {
-      console.error("Failed to fetch configs:", error);
-      toast.error(error?.message || "Failed to fetch configs");
-    },
   });
 };
